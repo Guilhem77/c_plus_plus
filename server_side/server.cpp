1463efd8c86abcfad9ff6bb512f8cd57a1a8d208
Guilhem77 <torgue.guilhem@gmail.com>
2018-02-10 10:05:18 +0100

    Add files via upload

--- server_side/server.cpp
+++ server_side/server.cpp
@@ -2,7 +2,7 @@
 
 Server::Server()
 {
-    // Création et disposition des widgets de la fenêtre
+    // Creation and disposition of windows widgets
     serverState = new QLabel;
     leaveButton = new QPushButton(tr("Quitter"));
     connect(leaveButton, SIGNAL(clicked()), qApp, SLOT(quit()));
@@ -14,18 +14,18 @@ Server::Server()
 
     setWindowTitle(tr("Server"));
 
-    // Gestion du serveur
+    // Server management
     server = new QTcpServer(this);
-    if (!server->listen(QHostAddress::Any, 50885)) // Démarrage du serveur sur toutes les IP disponibles et sur le port 50585
+    if (!server->listen(QHostAddress::Any, 50885)) // We start the server on all free IPs and port 50585
     {
-        // Si le serveur n'a pas été démarré correctement
+        // If the server didn't start correctly
         serverState->setText(tr("Le serveur n'a pas pu être démarré. Raison :<br />") + server->errorString());
     }
     else
     {
-        // Si le serveur a été démarré correctement
+        // If the server start correctly
         serverState->setText(tr("Le serveur a été démarré sur le port <strong>") + QString::number(server->serverPort()) + tr("</strong>.<br />Des clients peuvent maintenant se connecter."));
-        connect(server, SIGNAL(newConnection()), this, SLOT(nouvelleConnexion()));
+        connect(server, SIGNAL(newConnection()), this, SLOT(newConnection()));
     }
 
     messageLength = 0;
@@ -44,38 +44,36 @@ void Server::newConnection()
 
 void Server::receivedData()
 {
-    // 1 : on reçoit un paquet (ou un sous-paquet) d'un des clients
+    // 1 : We receive a package from one client
 
-    // On détermine quel client envoie le message (recherche du QTcpSocket du client)
+    // We determine which client send the message
     QTcpSocket *socket = qobject_cast<QTcpSocket *>(sender());
-    if (socket == 0) // Si par hasard on n'a pas trouvé le client à l'origine du signal, on arrête la méthode
+    if (socket == 0) // If we didn't find the client we stop the function
         return;
 
-    // Si tout va bien, on continue : on récupère le message
+    // If all is ok we get the message
     QDataStream in(socket);
 
-    if (messageLength == 0) // Si on ne connaît pas encore la taille du message, on essaie de la récupérer
+    if (messageLength == 0) // If we don't know the message length we try to get it
     {
-        if (socket->bytesAvailable() < (int)sizeof(quint16)) // On n'a pas reçu la taille du message en entier
+        if (socket->bytesAvailable() < (int)sizeof(quint16)) // We didn't receive the whole length message
              return;
 
-        in >> messageLength; // Si on a reçu la taille du message en entier, on la récupère
+        in >> messageLength; // If we receive the message length we get it
     }
 
-    // Si on connaît la taille du message, on vérifie si on a reçu le message en entier
-    if (socket->bytesAvailable() < messageLength) // Si on n'a pas encore tout reçu, on arrête la méthode
+    if (socket->bytesAvailable() < messageLength)
         return;
 
 
-    // Si ces lignes s'exécutent, c'est qu'on a reçu tout le message : on peut le récupérer !
+    // We can get the message
     QString message;
     in >> message;
 
 
-    // 2 : on renvoie le message à tous les clients
+    // 2 : We send the message to all clients
     sendToAll(message);
 
-    // 3 : remise de la taille du message à 0 pour permettre la réception des futurs messages
     messageLength = 0;
 }
 
@@ -83,9 +81,9 @@ void Server::logOutClient()
 {
     sendToAll(tr("<em>Un client vient de se déconnecter</em>"));
 
-    // On détermine quel client se déconnecte
+    // We determine which client has disconnected
     QTcpSocket *socket = qobject_cast<QTcpSocket *>(sender());
-    if (socket == 0) // Si par hasard on n'a pas trouvé le client à l'origine du signal, on arrête la méthode
+    if (socket == 0) // If we didn't find the client we stop the function
         return;
 
     clients.removeOne(socket);
@@ -95,17 +93,17 @@ void Server::logOutClient()
 
 void Server::sendToAll(const QString &message)
 {
-    // Préparation du paquet
+    // Package preparation
     QByteArray package;
     QDataStream out(&package, QIODevice::WriteOnly);
 
-    out << (quint16) 0; // On écrit 0 au début du paquet pour réserver la place pour écrire la taille
-    out << message; // On ajoute le message à la suite
-    out.device()->seek(0); // On se replace au début du paquet
-    out << (quint16) (package.size() - sizeof(quint16)); // On écrase le 0 qu'on avait réservé par la longueur du message
+    out << (quint16) 0; // we wirte 0 to reserve the place of message length
+    out << message; // We add the message
+    out.device()->seek(0); // We rewind to the beggining of the package
+    out << (quint16) (package.size() - sizeof(quint16)); // We write the length and remove the 0
 
 
-    // Envoi du paquet préparé à tous les clients connectés au serveur
+    // We send the package to all connected clients
     for (int i = 0; i < clients.size(); i++)
     {
         clients[i]->write(package);
